b58f91f28237daaedc8d89a49b31476ecf3071e9
JustanXC <105607465+JustanXC@users.noreply.github.com>
2022-05-16 02:50:22 +0700

    Add files via upload

---
+++ 2 sem 7 lab.cpp
@@ -0,0 +1,135 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <fstream>
+#include <iomanip>
+
+const int n = 2;
+using namespace std;
+
+struct Human
+{
+    string City;
+    int year;
+    int growth;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Human q[n];
+    Human z[n];
+    ifstream f("1.txt", ios::in);
+    if (!f.is_open()) {
+        cout << "Ошибка\n\n";
+        exit(0);
+    }
+    else {
+        cout << "Файл открыт\n\n";
+        //Блок записи данных в структуру
+        for (int i = 0; i < n; i++)
+        {
+            string temp;
+            f >> temp;
+            q[i].City = temp;
+            if (i == 0)
+                for (int j = 0; j < 5; j++)
+                {
+                    f >> q[j].year;
+                    f >> q[j].growth;
+                }
+            if (i == 1)
+                for (int h = 0; h < 5; h++)
+                {
+                    int x;
+                    f >> x;
+                    z[h].year = x;
+                    f >> x;
+                    z[h].growth = x;
+                }
+        }
+
+        cout << endl << endl << "\t               Таблица до внесения изменений" << endl << endl << "\t\t" << " |    " << "Город" << "    | " << setw(7) << "Год" << "    | " << setw(7) << "Прирост" << "  | " << endl;
+        cout << "\t\t" << " - - - - - - - - - - - - - - - - - - - -" << endl;
+
+        // Блок вывода таблицы матрицей
+        for (int i = 0; i < n; i++)
+        {
+            cout << "\t\t" << " | " << setw(11) << left << q[i].City << " | ";
+            if (i == 0)
+                for (int j = 0; j < 5; j++)
+                {
+                    cout << "   " << setw(7) << q[j].year << " |    " << setw(5) << q[j].growth << " | ";
+                    if (j < 4)
+                        cout << endl << "\t\t" << " | " << "            | ";
+                }
+            if (i == 1)
+                for (int h = 0; h < 5; h++)
+                {
+                    cout << "   " << setw(7) << z[h].year << " |    " << setw(5) << z[h].growth << " | ";
+                    if (h < 4)
+                        cout << endl << "\t\t" << " | " << "            | ";
+                }
+            cout << endl;
+            cout << "\t\t ---------------------------------------";
+            cout << endl;
+        }
+
+        cout << endl << endl;
+        int max1 = -99;
+        int max2 = -99;
+        // Блок максимального прироста населения
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0)
+                for (int j = 0; j < 5; j++)
+                {
+                    if (q[j].growth > max1)
+                        max1 = q[j].growth;
+                }
+            if (i == 1)
+                for (int h = 0; h < 5; h++)
+                {
+                    if (z[h].growth > max2)
+                        max2 = z[h].growth;
+                }
+        }
+
+        cout << endl << endl << "                         Таблица после внесения изменений" << endl << endl << "\t" << " |    " << "Город" << "    |     " << setw(7) << "Год" << "| " << setw(7) << "Прирост" << "  |  " << setw(7) << "Максимальный прирост" << " | " << endl;
+        cout << "\t" << " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -" << endl;
+
+        // Блок вывода таблицы матрицей
+        for (int i = 0; i < n; i++)
+        {
+            cout << "\t" << " | " << setw(11) << left << q[i].City << " | ";
+            if (i == 0)
+                for (int j = 0; j < 5; j++)
+                {
+                    cout << "   " << setw(7) << q[j].year << " |    " << setw(5) << q[j].growth << " |           ";
+                    if (j == 2)
+                        cout << setw(6) << max1 << "      | ";
+                    else
+                        cout << "            | ";
+                    if (j < 4)
+                        cout << endl << "\t" << " | " << "            | ";
+                }
+            if (i == 1)
+                for (int h = 0; h < 5; h++)
+                {
+                    cout << "   " << setw(7) << z[h].year << " |    " << setw(5) << z[h].growth << " |           ";
+                    if (h == 2)
+                        cout << setw(6) << max2 << "      | ";
+                    else
+                        cout << "            | ";
+                    if (h < 4)
+                        cout << endl << "\t" << " | " << "            | ";
+                }
+            cout << endl;
+            cout << "\t ---------------------------------------------------------------";
+            cout << endl;
+        }
+        cout << endl << endl << endl << endl;
+    }
+}
---
+++ 2SEM 8 LAB.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <Windows.h>
+#define k 10
+
+using namespace std;
+
+struct aero {
+	string num;
+	string start;
+	string end;
+};
+
+void XR() {
+	//Заполнение файла структурой
+	aero tab[k];
+	string s;
+	cout << "Введите\n\n";
+	for (int i = 0; i < k; i++) {
+		;
+		cout << "\nНомер рейса, пункт отпрвления, пункт прибытия (через пробел)\n";
+		cin >> tab[i].num;
+		cin >> tab[i].start;
+		cin >> tab[i].end;
+	}
+	system("cls");
+
+	//запись в файл
+
+	ofstream file("data.dat", ios::binary);
+	file.write((char*)&tab, sizeof(tab));
+	file.close();
+}
+void print(aero tmp[]) {
+	cout << "Список всех рейсов\n\n№ рейса" << setw(30) << "Пункт отправления" << setw(30) << "Пункт прибытия\n";
+	for (int i = 0; i < k; i++) {
+		cout << tmp[i].num << "\t\t\t" << tmp[i].start << "\t\t\t\t" << tmp[i].end << endl;
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	aero tmp[k];
+
+	// XR();
+
+	//чтение из файла
+
+	ifstream fin("data.dat", ios::binary);
+	if (fin.is_open()) {
+		cout << "Файл открыт\n\n";
+	}
+	else {
+		cout << "Ошибка чтения файла\n\n";
+		exit(404);
+	}
+	string usr; int count = 0;
+	cout << "Введите город отпрвления или прибытия для поиска: ";
+	cin >> usr;
+	for (int i = 0; i < k; i++) {
+		fin.read((char*)&tmp, sizeof(tmp));
+		if (tmp[i].start == usr || tmp[i].end == usr) {
+			cout << endl << "№ рейса" << setw(30) << "Пункт отправления" << setw(30) << "Пункт прибытия\n";
+			cout << tmp[i].num << "\t\t\t" << tmp[i].start << "\t\t\t\t" << tmp[i].end << endl;
+			count++;
+		}
+	}
+	fin.close();
+	if (count == 0) {
+		cout << "Такого рейса не найдено\n\n";
+	}
+	// print(tmp);
+	return 0;
+}
+
